eac12a454f34820ec032ce76df337f22bb49e749
Nicolas Osorio <osorionicolas95@gmail.com>
2023-08-09 00:58:53 +0200

    fix issues with security configuration

--- src/main/java/com/identicum/identity/demoapi/configuration/SecurityConfiguration.java
+++ src/main/java/com/identicum/identity/demoapi/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.identicum.identity.demoapi.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,24 +10,24 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.jboss.logging.Logger;
 
+@Configuration
 public class SecurityConfiguration {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     String issuerUri;
-    private static final Logger logger = Logger.getLogger(SecurityFilterChain.class);
+    private static final Logger logger = Logger.getLogger(SecurityConfiguration.class);
 
    @Bean
         public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-            RequestMatcher productMatcher = new AntPathRequestMatcher("/products");
+            RequestMatcher productMatcher = new AntPathRequestMatcher("/api/v1/products");
 
             http.authorizeHttpRequests(authorizeRequests -> {
                 authorizeRequests
-                    .requestMatchers(productMatcher).hasAuthority("SCOPE_product:read")
+                    .requestMatchers(productMatcher).hasAuthority("SCOPE_products")
                     .anyRequest().authenticated();
             }).oauth2ResourceServer(oauth2ResourceServer -> {
                 oauth2ResourceServer
                 .jwt(jwt -> {
-                    
                     jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri));
                 });
             });
